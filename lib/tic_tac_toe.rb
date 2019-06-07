599269dacb187807cc3f0be7829859fbf62756c8
Monique Vitche <mvitche7@gmail.com>
2019-06-07 02:53:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,7 +15,7 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-x
+
 
 def position_taken?(board, position)
   !(board[position].nil? || board[position] == " " || board[position] == "")
